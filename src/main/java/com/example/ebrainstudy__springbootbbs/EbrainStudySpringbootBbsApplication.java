0fbfdee3965e09f388abdc586d0aa9cb04ea5e38
yongwoojeong <jywoo9@gmail.com>
2022-12-26 16:47:19 +0900

    init

---
+++ src/main/java/com/example/ebrainstudy__springbootbbs/EbrainStudySpringbootBbsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ebrainstudy__springbootbbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbrainStudySpringbootBbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbrainStudySpringbootBbsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ebrainstudy__springbootbbs/EbrainStudySpringbootBbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ebrainstudy__springbootbbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbrainStudySpringbootBbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
