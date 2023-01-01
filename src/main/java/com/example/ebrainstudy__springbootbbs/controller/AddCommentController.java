de02bf27d71997d674aa3e0dfe9e03f68fb5337d
yongwoojeong <jywoo9@gmail.com>
2023-01-01 13:28:52 +0900

    Comment on comment controller,service

--- src/main/java/com/example/ebrainstudy__springbootbbs/controller/AddCommentController.java
+++ src/main/java/com/example/ebrainstudy__springbootbbs/controller/AddCommentController.java
@@ -6,12 +6,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+/**
+ * 댓글 입력 요청 처리하는 컨트롤러
+ */
 @Controller
 public class AddCommentController {
+
+	/**
+	 * 서비스 컴포넌트 의존성 주입
+	 */
 	private InsertCommentService insertCommentService;
 	public AddCommentController(InsertCommentService insertCommentService){
 		this.insertCommentService = insertCommentService;
 	}
+
+	/**
+	 * ArticleView 에서 입력받은 댓글을
+	 * POST 요청받아 서비스 컴포넌트로 보내는 컨트롤러
+	 * @param req
+	 * @param id
+	 * @return
+	 */
 	@PostMapping("/addComment")
 	public String addComment(HttpServletRequest req, @RequestParam int id){
 		String commentContent = req.getParameter("new_comment");
--- src/main/java/com/example/ebrainstudy__springbootbbs/service/InsertCommentService.java
+++ src/main/java/com/example/ebrainstudy__springbootbbs/service/InsertCommentService.java
@@ -4,24 +4,53 @@ import com.example.ebrainstudy__springbootbbs.comment.CommentDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * 댓글 내용을 전달받아 DAO 통해 INSERT 처리하는 서비스 컴포넌트
+ */
 @Service
 public class InsertCommentService {
+
+	/**
+	 * INSERT에 필요한 DAO 객체 의존성 주입
+	 */
 	private final CommentDAO commentDAO;
+	/**
+	 * 댓글내용
+	 */
 	private String commentContent;
+	/**
+	 * 댓글이 등록될 게시글 ID (FK)
+	 */
 	private int articleId;
+
+	/**
+	 * 생성자
+	 * @param commentDAO
+	 */
 	@Autowired
 	public InsertCommentService(CommentDAO commentDAO){
 		this.commentDAO = commentDAO;
 	}
 
+	/**
+	 * 댓글 내용을 전달받는 세터메서드
+	 * @param commentContent
+	 */
 	public void setCommentContent(String commentContent) {
 		this.commentContent = commentContent;
 	}
 
+	/**
+	 * 게시글 ID 를 전달받는 세터메서드
+	 * @param articleId
+	 */
 	public void setArticleId(int articleId) {
 		this.articleId = articleId;
 	}
 
+	/**
+	 * DAO 통해 코멘트 테이블에 INSERT 해주는 메서드
+	 */
 	public void process(){
 		commentDAO.insertComment(commentContent, articleId);
 	}
