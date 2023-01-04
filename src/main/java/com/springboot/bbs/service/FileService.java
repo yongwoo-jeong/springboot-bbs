6b4b42ad4bdde62b38818282e689ad738186b203
yongwoojeong <jywoo9@gmail.com>
2023-01-04 09:49:56 +0900

    Add file utils

--- src/main/java/com/springboot/bbs/service/FileService.java
+++ src/main/java/com/springboot/bbs/service/FileService.java
@@ -1,6 +1,7 @@
 package com.springboot.bbs.service;
 
 import com.springboot.bbs.repository.FileRepository;
+import com.springboot.bbs.utils.FileUtils;
 import com.springboot.bbs.vo.FileVO;
 import java.io.File;
 import java.io.IOException;
@@ -8,43 +9,69 @@ import java.math.BigInteger;
 import java.util.Date;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+/**
+ * 파일 서비스 컴포넌트
+ */
 @Service
 @RequiredArgsConstructor
 public class FileService {
+
+	/**
+	 * 파일 저장될 서버경로
+	 */
+	@Value("${dev.file.localPath}")
+	private String FILEPATH;
+
+	/**
+	 * 파일레포지토리 생성자
+	 */
 	private final FileRepository fileRepository;
 
-	@Value("${dev.file.localPath}")
-	private String serverFilePath;
+	/**
+	 * 로거
+	 */
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+	private FileUtils fileUtils;
+
+	/**
+	 * 파일 추가 서비스
+	 * @param fileList List of MultipartFile
+	 * @param articleId 게시글 id
+	 */
 	public void insertFileService(List<MultipartFile> fileList, Integer articleId){
 		for (MultipartFile file : fileList){
-			// 파일명없거나 (null) 파일 사이즈가 0일 경우 건너뛰기
-			if (file.getOriginalFilename() == null || file.getSize() == 0) {
+			if (FileUtils.isFileEmpty(file)) {
 				continue;
 			}
+			// 유저가 업로드한 파일명
 			String fileNameOriginal = file.getOriginalFilename();
 			// 파일명 + 현재시간으로 서버에 저장될 파일명 결정
 			String fileNameServer = file.getOriginalFilename().split("\\.")[0]+(new Date()).toString().replace(" ","_");
-			String filePath = serverFilePath;
+			String filePath = FILEPATH;
 			BigInteger fileSize = BigInteger.valueOf(file.getSize());
 			// 파일 확장자명
 			String fileExtension = file.getOriginalFilename().split("\\.")[1];
 			// DB에 저장될 파일 객체생성
-			FileVO newFile = FileVO.builder().nameOnServer(fileNameServer).nameOriginal(fileNameOriginal)
-					.articleId(articleId).filePath(filePath)
-					.fileSize(fileSize).fileExtension(fileExtension)
-					.build();
-			// 서버 저장될 파일 객체
+			FileVO newFile = FileVO.builder()
+									.nameOnServer(fileNameServer)
+									.nameOriginal(fileNameOriginal)
+									.articleId(articleId).filePath(filePath)
+									.fileSize(fileSize).fileExtension(fileExtension)
+									.build();
+			// 서버 저장될 파일
 			File fileToServer = new File(filePath+fileNameOriginal);
 			try {
 				file.transferTo(fileToServer);
 				fileRepository.insertFile(newFile);
 			} catch (IOException e) {
-				e.printStackTrace();
+				logger.error(String.valueOf(e));
 			}
 		}
 	}
---
+++ src/main/java/com/springboot/bbs/utils/FileUtils.java
@@ -0,0 +1,23 @@
+package com.springboot.bbs.utils;
+
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * 파일 관련 유틸
+ */
+public class FileUtils {
+
+	/**
+	 * 빈 파일인지 확인
+	 * @param file MultipartFile
+	 * @return boolean
+	 */
+	public static boolean isFileEmpty(MultipartFile file){
+		// 파일명없거나(null) 파일 사이즈가 0일 경우 빈 파일
+		if (file.getOriginalFilename() == null || file.getSize() == 0) {
+			return true;
+		}
+		return false;
+	}
+
+}
